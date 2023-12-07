8f487b49951b8adbff6dc6e5b414cf0ce1ab20f3
Ruben <111981761+R7b3nv0k@users.noreply.github.com>
2023-12-07 09:30:07 +0100

    Update feleadat.py

--- feleadat.py
+++ feleadat.py
@@ -9,3 +9,9 @@ name = st.text_input("Enter Your name", "Type Here ...")
 email = st.text_input("Enter your email", "Type Here ...")
 kor= st.text_input("Enter your age", "Type Here ...")
 gender = st.radio("Select Gender: ", ('Male', 'Female'))
+
+name = st.text_input(".text_input: Enter Your name", "Type Here ...")
+
+if(st.button('.button: Submit')):
+	result = name.title()
+	st.success(result)
