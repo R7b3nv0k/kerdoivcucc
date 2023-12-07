cc573a76d42e45b790be6cd7a621794c0d5dbf63
Ruben <111981761+R7b3nv0k@users.noreply.github.com>
2023-12-07 08:31:12 +0100

    Update feleadat.py

--- feleadat.py
+++ feleadat.py
@@ -5,5 +5,5 @@ st.header("This is a header")
 
 st.text("Kerdoiv!!!")
 
-name = st.text_input(".text_input: Enter Your name", "Type Here ...")
-email = st.text_input(".text_input: Enter ur email", "Type Here ...")
+name = st.text_input("Enter Your name", "Type Here ...")
+email = st.text_input("Enter ur email", "Type Here ...")
