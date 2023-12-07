dddaae11acc219a8bfaa1cf35d2f72e0de0ef68e
Ruben <111981761+R7b3nv0k@users.noreply.github.com>
2023-12-07 08:40:30 +0100

    Update feleadat.py

--- feleadat.py
+++ feleadat.py
@@ -1,9 +1,11 @@
 import streamlit as st
 
-st.header("This is a header") 
+st.header("Kerdoív") 
 
 
 st.text("Kerdoiv!!!")
 
 name = st.text_input("Enter Your name", "Type Here ...")
-email = st.text_input("Enter ur email", "Type Here ...")
+email = st.text_input("Enter your email", "Type Here ...")
+kor= st.text_input("Írd be a korord", "Type Here ...")
+gender = st.radio("Select Gender: ", ('Male', 'Female'))
